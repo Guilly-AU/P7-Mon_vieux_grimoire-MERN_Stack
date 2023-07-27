2b0398f32a476e90f075ff572f9c615fb368c1ff
guilly <guillaume.damenez@gmail.com>
2023-07-27 17:09:17 +0800

    Controller + route Book average rating

--- routes/book.js
+++ routes/book.js
@@ -8,7 +8,7 @@ const optimizedImg = require("../middleware/sharp");
 const bookCtrl = require("../controllers/book");
 
 router.post("/", auth, multer, optimizedImg, bookCtrl.createBook);
-router.post("/:id/rating", bookCtrl.rateBook);
+router.post("/:id/rating", auth, bookCtrl.rateBook);
 router.put("/:id", auth, multer, optimizedImg, bookCtrl.modifyBook);
 router.delete("/:id", auth, bookCtrl.deleteBook);
 router.get("/bestrating", bookCtrl.getBestBooks);
