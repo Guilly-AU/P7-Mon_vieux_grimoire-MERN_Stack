a752ef05be2d707ae0666deb92f93d711c9054c7
guilly <guillaume.damenez@gmail.com>
2023-07-27 17:08:44 +0800

    controller + route book average rating

--- controllers/book.js
+++ controllers/book.js
@@ -1,3 +1,4 @@
+const { error } = require("console");
 const Book = require("../models/Book");
 const fs = require("fs");
 
@@ -95,22 +96,47 @@ exports.getBestBooks = (req, res, next) => {
 };
 
 exports.rateBook = (req, res, next) => {
-  if (!req.body.userId) {
-    res.status(401).json({ message: "Not authorized" });
-  } else {
-    const newRating = {
-      userId: req.body.userId,
-      grade: req.body.rating,
-    };
+  const user = req.body.userId;
+  const userRating = req.body.rating;
 
-    Book.findOneAndUpdate(
-      { _id: req.params.id, "ratings.userId": { $ne: req.body.userId } },
-      { $push: { ratings: newRating } },
-      { new: true }
-    )
-      .then((updatedBook) => {
-        res.status(200).json(updatedBook);
+  if (user !== req.auth.userId) {
+    return res.status(401).json({ message: "Not authorized" });
+  } else {
+    Book.findOne({ _id: req.params.id })
+      .then((book) => {
+        if (userRating < 0 || userRating > 5) {
+          return res
+            .status(400)
+            .json({ message: "La note doit être comprise entre 0 et 5!" });
+        }
+        if (book.ratings.find((rating) => rating.userId === user)) {
+          return res.status(401).json({ message: "Book already rated!" });
+        } else {
+          const newRating = {
+            userId: user,
+            grade: userRating,
+          };
+
+          const sumRatings = book.ratings.reduce(
+            (sum, rating) => sum + rating.grade,
+            0
+          );
+
+          const updateAverageRating =
+            (sumRatings + userRating) / (book.ratings.length + 1);
+
+          Book.findOneAndUpdate(
+            { _id: req.params.id, "ratings.userId": { $ne: user } },
+            {
+              $push: { ratings: newRating },
+              averageRating: updateAverageRating.toFixed(2),
+            },
+            { new: true }
+          )
+            .then((updatedBook) => res.status(201).json(updatedBook))
+            .catch((error) => res.status(401).json({ error }));
+        }
       })
-      .catch((error) => res.status(400).json({ error }));
+      .catch((error) => res.status(401).json({ error }));
   }
 };
